2bb934bc46a2a3b47aed1ec915b930080b7f85e2
kieuphan4 <kieuphan530@gmail.com>
2022-01-14 00:01:34 -0600

    commit tree 7e83fd14c36358e861be055e3774e9e7b8ba1580

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,16 +3,37 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
-
+  
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+  setFoods(newFoodArray);
   }
+
+  function handleLiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+
+  const foodElements = foods.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+    </li>
+  ));
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
+      <ul>{foodElements}</ul>
     </div>
   );
 }
